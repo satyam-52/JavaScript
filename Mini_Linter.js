2448bb26898ea2cd1006d176dae9771968f67405
Satyam Dua <69812664+satyam-52@users.noreply.github.com>
2020-09-19 14:20:06 +0530

    Create Mini_Linter.js
    A small scale copy of linter used in Grammerly.

---
+++ Mini_Linter.js
@@ -0,0 +1,34 @@
+let story = 'Last weekend, I took literally the most beautiful bike ride of my life. The route is called "The 9W to Nyack" and it actually stretches all the way from Riverside Park in Manhattan to South Nyack, New Jersey. It\'s really an adventure from beginning to end! It is a 48 mile loop and it basically took me an entire day. I stopped at Riverbank State Park to take some extremely artsy photos. It was a short stop, though, because I had a really long way left to go. After a quick photo op at the very popular Little Red Lighthouse, I began my trek across the George Washington Bridge into New Jersey.  The GW is actually very long - 4,760 feet! I was already very tired by the time I got to the other side.  An hour later, I reached Greenbrook Nature Sanctuary, an extremely beautiful park along the coast of the Hudson.  Something that was very surprising to me was that near the end of the route you actually cross back into New York! At this point, you are very close to the end.';
+
+let overusedWords = ['really', 'very', 'basically'];
+
+let unnecessaryWords = ['extremely', 'literally', 'actually' ];
+
+let storyWords = story.split(' ');
+// console.log(storyWords.length);
+const betterWords = storyWords.filter(word => {
+  return !unnecessaryWords.includes(word)
+});
+// console.log(betterWords.length);
+let really = 0, very = 0, basically = 0;
+for (word of betterWords) {
+  if (word === 'really') {
+    really++;
+  } else if (word === 'very') {
+    very++;
+  } else if (word === 'basically') {
+    basically++;
+  }
+}
+console.log(`OVERUSED WORDS:\nReally appears: ${really} times.`);
+console.log(`Very appears: ${very} times.`);
+console.log(`Basically appears: ${basically} times.`);
+let sentences = 0;
+for(word of betterWords) {
+  if (word[0] === '.' || word[word.length-1] === '.' || word[0] === '!' || word[word.length-1] === '!') {
+    sentences++;
+  }
+}
+console.log(`\nNumber of sentences: ${sentences}`);
+console.log(`Number of words: ${betterWords.length}`);
+console.log(`\n\nSTORY:\n${betterWords.join(' ')}`);
